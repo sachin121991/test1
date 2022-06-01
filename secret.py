7b39a387144d705a458cd6b043167e24f1ef0694
sachin121991 <41074811+sachin121991@users.noreply.github.com>
2022-06-01 22:23:49 +0530

    Create secret.py

---
+++ secret.py
@@ -0,0 +1,69 @@
+
+import gzip
+import json
+import base64
+import boto3
+
+
+smtp_server = "smtp.gmail.com"
+smtp_port = 587
+smtp_username = "sachin@nclouds.com"
+smtp_passwword = "lmxmuaffkwdtcqtl"
+
+send_to = ['sachin.kapoor1991@gmail.com']
+cc = ['kapoor.sachin1991@gmaail.com']
+bcc = ['shivani@nclouds.com']
+send_from = "sachin@nclouds.com"
+
+
+
+def respond(err, res=None):
+    return {
+        'statusCode': '400' if err else '200',
+        'body': err.message if err else json.dumps(res),
+        #.encode('utf8'),
+        'headers': {
+            'Content-Type': 'application/json',
+            #'Content-Type': 'text',
+        },
+    }
+
+
+def lambda_handler(event, context):
+    
+
+    cw_data = event['awslogs']['data']
+    #cw_data = str(event['awslogs']['data'])
+    #cw_logs = gzip.GzipFile(fileobj=BytesIO(base64.b64decode(cw_data, validate=True))).read()
+    compressed_payload = base64.b64decode(cw_data)
+    uncompressed_payload = gzip.decompress(compressed_payload)
+    payload = json.loads(uncompressed_payload)
+    log_events = payload['logEvents']
+    #log_events = json.loads(cw_logs)
+    for log_event in log_events:
+        a = log_event['message']
+    print(a)
+    log_events_data = json.loads(a)
+    
+      
+    
+    source = log_events_data['eventSource']
+    nameevent = log_events_data['eventName']
+    user = log_events_data['userIdentity']['principalId']
+    secret = log_events_data['requestParameters']['secretId']
+    msg = "Hi Team,     We have detected the %s event from AWS Secret Manager Service with below details:    Event Source- %s    Event Name- %s    User- %s  Secret-Id- %s " % (nameevent,source,nameevent,user,secret)
+    subj = "Attention! Describe Secret Event Detected by user- %s" % (user)    
+   
+    if 'DescribeSecret' in nameevent:
+        MY_SNS_TOPIC_ARN = 'arn:aws:sns:us-east-1:695292474035:sachin-secret'
+        sns_client = boto3.client('sns')
+        sns_client.publish(
+        TopicArn = MY_SNS_TOPIC_ARN,
+        Subject = subj,
+        Message = msg
+        )
+        
+        return respond(None, "Thanks for using this command. Sending Mail.....")
+    else:
+
+        return respond(None, "Thanks for using this fucntion but event detected is diff:%s" % (nameevent))
